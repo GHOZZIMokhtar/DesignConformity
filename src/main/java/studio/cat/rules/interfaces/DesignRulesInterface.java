7b05af39b2ad1dbae60f48692f786930706a0c7b
Mokhtar Ghozzi <mghozzi@pws.com>
2017-09-14 14:50:08 +0200

    Ajout du JAVA Doc

--- src/main/java/studio/cat/rules/interfaces/DesignRulesInterface.java
+++ src/main/java/studio/cat/rules/interfaces/DesignRulesInterface.java
@@ -4,6 +4,11 @@ import org.json.simple.JSONObject;
 
 public interface DesignRulesInterface {
 	
+	/**
+	 * Méthode permettant de vérifier si le tableau des pages alternatives est vide
+	 * @param innerObjectInPage le tableau JSON d'une page
+	 * @return true si le tableau des pages alternative est vide, false si non
+	 */
 	Boolean isPageAlternativeEmpty(JSONObject innerObjectInPage);
 
 }
