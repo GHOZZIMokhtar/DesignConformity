817180824fd337084c621dc6f69945a154d90bb8
Mokhtar Ghozzi <mghozzi@pws.com>
2017-09-14 13:11:02 +0200

    Premier commit

---
+++ src/main/java/main/MainDesignConformity.java
@@ -0,0 +1,58 @@
+package main;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class MainDesignConformity {
+ 
+	public static void main(String[] args) {
+
+		try {
+			// read the json file
+			FileReader reader = new FileReader(args[0]); // Récupération du fichier JSON à manipuler lors de l'exécution du JAR
+
+			JSONParser jsonParser = new JSONParser();
+			JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+
+			// get a design name from the JSON object
+			String firstName = (String) jsonObject.get("name");
+			System.out.println("The design name is: " + firstName);
+
+			// get design id from the JSON object
+			String id =  (String) jsonObject.get("id");
+			System.out.println("The design id is: " + id);
+
+			// get an array from the JSON object
+			JSONArray lang= (JSONArray) jsonObject.get("pages"); 
+			System.out.println("The number of page in this design is : "+lang.size());
+			Iterator<?> iterateur = lang.iterator();
+			File reportFile=new File(args[1]); // récupérer le chemin du fichier de sortie à partir des paramètres passées à l'éxécution du JAR
+			reportFile.createNewFile();
+			FileWriter fileWriter=new FileWriter(reportFile);
+			// take each value from the json array separately
+			while (iterateur.hasNext()) {
+				JSONObject innerObj = (JSONObject) iterateur.next();
+				System.out.println("page id "+ innerObj.get("id") + ", page name "+ innerObj.get("name") +  ", alternative pages "+ innerObj.get("alt_pages") + 
+						", page backgroung color " + innerObj.get("bgcolor"));
+				fileWriter.write("page id "+ innerObj.get("id") + ", page name "+ innerObj.get("name") +  ", alternative pages "+ innerObj.get("alt_pages") + 
+						", page backgroung color " + innerObj.get("bgcolor")+"\n");  // écrire une ligne dans le fichier resultat.txt 
+				fileWriter.write("\n"); // forcer le passage à la ligne
+			}
+			fileWriter.close(); // fermer le fichier à la fin des traitements
+		} catch (ParseException ex) {
+			ex.printStackTrace();
+		} catch (NullPointerException ex) {
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
